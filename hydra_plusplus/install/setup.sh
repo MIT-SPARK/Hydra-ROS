1a08764b877b3aa98e86a5d837a7ffdd4c7b480b
Nathan Hughes <nathan.h.hughes@gmail.com>
2026-07-30 15:13:03 +0000

    (wip) add model downloads and crisp model configs

--- hydra_plusplus/install/setup.sh
+++ hydra_plusplus/install/setup.sh
@@ -3,7 +3,17 @@
 pkg_dir="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)")"
 shell="$(basename "${SHELL}")"
 
+function download_model() {
+    if [[ ! -e $1 ]]; then
+        echo "Downloading '$1'..."
+        mkdir -p $(dirname $1)
+        "${pkg_dir}/env/bin/python3" -m gdown -O $1 $2
+    fi
+}
+
 install_crisp=true
+install_models=true
+install_shell=true
 
 while :; do
     case $1 in
@@ -11,8 +21,16 @@ while :; do
             install_crisp=false
             shift
             ;;
+        --no-models)
+            install_models=false
+            shift
+            ;;
+        --no-shell)
+            install_shell=false
+            shift
+            ;;
         -h|-?|--help)
-            echo "Usage: setup.sh [--no-crisp]"
+            echo "Usage: setup.sh [--no-crisp] [--no-models] [--no-shell]"
             exit
             ;;
         *)
@@ -27,21 +45,31 @@ if [ "$install_crisp" = true ]; then
     "${pkg_dir}/env/bin/python3" -m pip install git+https://github.com/MIT-SPARK/CRISP
 fi
 
-user_config="$HOME/.bashrc"
-case $shell in
-    bash)
-        echo "Exporting variables to .bashrc"
-        user_config="$HOME/.bashrc"
-        ;;
-    zsh)
-        echo "Exporting variables to .zshrc"
-        user_config="$HOME/.zshrc"
-        ;;
-    *)
-        echo "Unknown shell '$shell'!"
-        exit
-esac
+if [ "$install_models" = true ]; then
+    echo "Downloading models to: ${pkg_dir}/models"
+    python3 -m virtualenv --system-site-packages "${pkg_dir}/env"
+    "${pkg_dir}/env/bin/python3" -m pip install gdown
+    download_model "${pkg_dir}/models/202511201549_Z4722/checkpoint.pth" https://drive.google.com/file/d/1xHvoSCOlTEX-pweAtjoaiRlM74YK5sgA/view?usp=sharing
+    download_model "${pkg_dir}/models/202601240250_PY271/checkpoint.pth" https://drive.google.com/file/d/1a5oFGUhVpxTc54Ej7TR7AYJQOoUMMWlW/view?usp=sharing
+fi
 
-echo "export PATH="'$PATH:'"${pkg_dir}/bin" >> "$user_config"
-echo "export HYDRAPP_ENV=${pkg_dir}/env" >> "$user_config"
-echo "export HYDRAPP_MODEL_DIR=${pkg_dir}/models/" >> "$user_config"
+if [ "$install_shell" = true ]; then
+    user_config="$HOME/.bashrc"
+    case $shell in
+        bash)
+            echo "Exporting variables to .bashrc"
+            user_config="$HOME/.bashrc"
+            ;;
+        zsh)
+            echo "Exporting variables to .zshrc"
+            user_config="$HOME/.zshrc"
+            ;;
+        *)
+            echo "Unknown shell '$shell'!"
+            exit
+    esac
+
+    echo "export PATH="'$PATH:'"${pkg_dir}/bin" >> "$user_config"
+    echo "export HYDRAPP_ENV=${pkg_dir}/env" >> "$user_config"
+    echo "export HYDRAPP_MODEL_DIR=${pkg_dir}/models/" >> "$user_config"
+fi
